669685a10adafcfce39df0f7cee348f4768e4f8f
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-13 09:47:35 +0530

    [Akshay]ADDED: code to demonstrate toString method

---
+++ toString.js
@@ -0,0 +1,13 @@
+function getMessage(msg) {
+    var name = msg;
+    // display name to console
+    
+    console.log(`Hello ${name}`);
+}
+
+//print the native code
+console.log(getMessage.toString());
+
+let num = 10000;
+console.log(num.toString());
+
