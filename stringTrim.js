a0efc7ff3dddec460a461eede896978cf31ed582
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-13 12:09:56 +0530

    [Akshay]ADDED: code to demonstrate string trim method

---
+++ stringTrim.js
@@ -0,0 +1,11 @@
+let str1="      ||demoString||      ";
+
+console.log(str1.length); 
+str1 = str1.trimStart();
+
+console.log(str1.length);
+
+str1 = str1.trimEnd(); 
+console.log(str1.length);
+
+console.log(str1);
