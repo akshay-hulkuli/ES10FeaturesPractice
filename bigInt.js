5505327eb658d9c03b7ea7f7a693610337e2bb1c
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-13 09:01:35 +0530

    [Akshay]ADDED: demonstration to bigInt datatype

---
+++ bigInt.js
@@ -0,0 +1,11 @@
+let maxNumber  = Number.MAX_SAFE_INTEGER;
+
+console.log(maxNumber);
+
+let temp = maxNumber + 100;
+console.log(temp * 2);
+console.log(typeof(temp))
+
+let bigIntNum = 1002834908234802980824n;
+console.log(bigIntNum);
+console.log(typeof(bigIntNum))
